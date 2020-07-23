f8a4c6b5e3e1b54c51bbb0e49d85cd8711214b1a
soumaxetuirk <54876219+soumaxetuirk@users.noreply.github.com>
2020-07-24 03:44:39 +0530

    release 4

--- Voice Commander/mod/mod.lua
+++ Voice Commander/mod/mod.lua
@@ -1,122 +1,93 @@
-function Vcmd(lvalue)
-	if lvalue == "Thanks" then
-		managers.player:local_player():sound():say("s05x_sin",true,true)
-	
-	elseif lvalue == "Let's go" then
-		managers.player:local_player():sound():say("g13",true,true)
-		
-	elseif lvalue == "Come with Me" then
-		managers.player:local_player():sound():say("p20",true,true)	
-	
-	elseif lvalue == "Right Place" then
-		managers.player:local_player():sound():say("g14",true,true)	
-	
-	elseif lvalue == "Right way" then
-		managers.player:local_player():sound():say("g12",true,true)	
-	
-	elseif lvalue == "Wrong way" then
-		managers.player:local_player():sound():say("g11",true,true)	
-	
-	elseif lvalue == "Get In" then
-		managers.player:local_player():sound():say("g08",true,true)	
-	
-	elseif lvalue == "Hurry" then
-		managers.player:local_player():sound():say("g09",true,true)	
-	
-	elseif lvalue == "love it" then
-		managers.player:local_player():sound():say("g60",true,true)	
-	
-	elseif lvalue == "fu@#***" then
-		managers.player:local_player():sound():say("g29",true,true)	
-	
-	elseif lvalue == "Ammo bag" then
-		managers.player:local_player():sound():say("g81x_plu",true,true)	
-	
-	elseif lvalue == "Medic bag" then
-		managers.player:local_player():sound():say("g80x_plu",true,true)
-
-	elseif lvalue == "HELP" then
-		managers.player:local_player():sound():say("f11e_plu",true,true)
-	
-	elseif lvalue == "Keycard Here" then
-		managers.player:local_player():sound():say("v10",true,true)	
-	
-	elseif lvalue == "Crowbar Here" then
-		managers.player:local_player():sound():say("v57",true,true)
-	
-	elseif lvalue == "careful" then
-		managers.player:local_player():sound():say("g10",true,true)
-	
-	elseif lvalue == "watch civillians" then
-		managers.player:local_player():sound():say("g27",true,true)
-	
-	elseif lvalue == "Takeout camers" then
-		managers.player:local_player():sound():say("g25",true,true)
-	
-	elseif lvalue == "Use cable tie" then
-		managers.player:local_player():sound():say("g26",true,true)
-	
-	elseif lvalue == "Keep Defend" then
-		managers.player:local_player():sound():say("g16",true,true)
-	
-	elseif lvalue == "Get Out" then
-		managers.player:local_player():sound():say("g07",true,true)
-	
-	elseif lvalue == "Get Moving" then
-		managers.player:local_player():sound():say("p15",true,true)
-	
-	elseif lvalue == "we are done" then
-		managers.player:local_player():sound():say("v08",true,true)
-	
-	elseif lvalue == "I NEED  >>   " then
-		local Addmenu = QuickMenu:new( menu_title, "I need >>", menu_options2)
-		Addmenu:Show()
-		
-	elseif lvalue == "Back" then
-		local Menu = QuickMenu:new( menu_title, "by Soum_AxetuirK", menu_options )
-		Menu:Show()
-
-	elseif lvalue == "Stealth   >>   " then
-		local Addmenu = QuickMenu:new( menu_title, "Stealth >>", menu_options3 )
-		Addmenu:Show()
-
-	elseif lvalue == "Loud  >>   " then
-		local Addmenu = QuickMenu:new( menu_title, "Loud >>", menu_options4 )
-		Addmenu:Show()
-		end
+function play(ID)
+	managers.player:local_player():sound():say(ID,true,true)
+	active = 0
+end
+function convert(lvalue)
+	if lvalue == "Back" then qmenu(1)
+	elseif lvalue == "Exit" then 
+		qmenu (0)
+		active = 0
+	elseif lvalue == "I NEED  >>   " then qmenu(2)	
+	elseif lvalue == "Stealth   >>   " then qmenu(3)	 
+	elseif lvalue == "Loud  >>   " then qmenu(4)
+	elseif lvalue == "Thanks" then play("s05x_sin")
+	elseif lvalue == "Let's go" then play("g13")
+	elseif lvalue == "Come with Me" then play("f38_any")
+	elseif lvalue == "Here it is" then play("v04")
+	elseif lvalue == "Right way" then play("g12")	
+	elseif lvalue == "Wrong way" then play("g11")	
+	elseif lvalue == "Get In" then play("g08")	
+	elseif lvalue == "Hurry" then play("g09")	
+	elseif lvalue == "love it" then play("g60")	
+	elseif lvalue == "fu@#***" then play("g29")	
+	elseif lvalue == "Ammo bag" then play("g81x_plu")	
+	elseif lvalue == "Medic bag" then play("g80x_plu")
+	elseif lvalue == "HELP" then play("f11e_plu")
+	elseif lvalue == "Keycard Here" then play("v10")	
+	elseif lvalue == "Crowbar Here" then play("v57")
+	elseif lvalue == "careful" then play("g10")
+	elseif lvalue == "watch civillians" then play("g27")
+	elseif lvalue == "Takeout camers" then play("g25")
+	elseif lvalue == "Use cable ties" then play("g26")
+	elseif lvalue == "Kill'em all" then play("v18")
+	elseif lvalue == "Get Out" then play("g07")
+	elseif lvalue == "get moving" then play("g18")
+	elseif lvalue == "I will wait" then play("p32")
+	elseif lvalue == "we are done" then play("v08")
+	elseif lvalue == "open door" then play("v15")
+	elseif lvalue == "Drill jammed" then play("d01x_sin")
+	elseif lvalue == "escape this way" then play("v26")
+	elseif lvalue == "turret here" then play("v45")
+	end
 end
 
-local menu_title = ("Voice Commander V3")
-local message_list = {"Thanks", "Let's go", "Come with Me", "Right Place", "Right way", "Wrong way", "Get In", "Hurry", "love it", "fu@#***", "I NEED  >>   ", "Stealth   >>   ", "Loud  >>   ", "Keycard Here", "Crowbar Here","EXIT"}
-local message_list2 = {"Ammo bag", "Medic bag", "HELP","Back","Exit"}
-local message_list3 = {"careful", "watch civillians", "Takeout camers","Use cable ties","Back","Exit"}
-local message_list4 = {"Keep Defend", "Get Out", "Get Moving","we are done","Back","Exit"}
-_G.menu_options = {}
- 
-	for i=1,#message_list do
-		menu_options[i] ={text = message_list[i], data = message_list[i], callback = Vcmd}
+function qmenu(lcode)
+	local menu_title = ("Voice Commander V4")
+	local message_list = {"Thanks", "Let's go", "Come with Me", "Here it is", "Right way", "Wrong way", "Get In", "Hurry", "love it", "fu@#***", "I NEED  >>   ", "Stealth   >>   ", "Loud  >>   ", "Drill jammed", "Keycard Here", "Crowbar Here","Exit"}
+	local needmenu = {"Ammo bag", "Medic bag", "HELP","Exit"}
+	local stealthmenu = {"careful", "open door","watch civillians", "Takeout camers","Use cable ties","I will wait","Exit"}
+	local loudmenu = {"Kill'em all", "Get Out","turret here", "get moving","we are done","escape this way", "Exit"}
+	_G.menu_options = {}
+			for i=1,#message_list do
+			menu_options[i] ={text = message_list[i], data = message_list[i], callback = convert}
+			end
+	_G.menu_options2 = {}
+			for i=1,#needmenu do
+			menu_options2[i] ={text = needmenu[i], data = needmenu[i], callback = convert}
+			end
+	_G.menu_options3 = {}
+			for i=1,#stealthmenu do
+			menu_options3[i] ={text = stealthmenu[i], data = stealthmenu[i], callback = convert}
+			end
+	_G.menu_options4 = {}
+			for i=1,#loudmenu do
+			menu_options4[i] ={text = loudmenu[i], data = loudmenu[i], callback = convert}
+			end
+	if lcode == 1 then
+		Menu = QuickMenu:new( menu_title, "By Soum_AxetuirK", menu_options)
+		Menu:Show()
+	elseif lcode == 2 then
+		active = 2
+		Menu = QuickMenu:new( menu_title2, "I NEED  >>", menu_options2)
+		Menu:Show()
+	elseif lcode == 3 then
+		active = 2
+		Menu = QuickMenu:new( menu_title, "Stealth   >>", menu_options3)
+		Menu:Show()
+	elseif lcode == 4 then
+		active = 2
+		Menu = QuickMenu:new( menu_title, "Loud  >>", menu_options4)
+		Menu:Show()
+	elseif lcode == 0 then
+		Menu:Hide()	
 	end
-_G.menu_options2 = {}
- 
-	for i=1,#message_list2 do
-		menu_options2[i] ={text = message_list2[i], data = message_list2[i], callback = Vcmd}
-	end
-_G.menu_options3 = {}
- 
-	for i=1,#message_list3 do
-		menu_options3[i] ={text = message_list3[i], data = message_list3[i], callback = Vcmd}
-	end
-_G.menu_options4 = {}
- 
-	for i=1,#message_list4 do
-		menu_options4[i] ={text = message_list4[i], data = message_list4[i], callback = Vcmd}
-	end	
+end
 
-_G.Menu = QuickMenu:new( menu_title, "by Soum_AxetuirK", menu_options )
 
-if Menu.visible then 
-    Menu:Hide()
-else
-    Menu:Show()
+if  active == nil or active == 0 then
+		active = 1
+		qmenu(1)	
+elseif active == 2 then
+		qmenu(0)
 end
 
